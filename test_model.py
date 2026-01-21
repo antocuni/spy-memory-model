06f214b9502e9dd2f5f72c0d82d6fac71ab1d66d
Antonio Cuni <anto.cuni@gmail.com>
2026-01-22 00:06:14 +0100

    simplify

--- test_model.py
+++ test_model.py
@@ -14,17 +14,10 @@ from spyruntime import (
 )
 from model import spy_object, ObjectObject
 
-# prebuilt values to be used in tests, just to make typing easier
-v0 = i32(0)
-v1 = i32(1)
-v2 = i32(2)
-v3 = i32(3)
-v4 = i32(4)
-
 
 def test_spy_type_value():
-    x = v1
-    y = v2
+    x = i32(1)
+    y = i32(2)
     assert x == i32(1)
     assert x != y
     assert get_type(x) is i32
@@ -37,20 +30,20 @@ def test_struct():
         x: i32
         y: i32
 
-    p = Point(x=v1, y=v2)
-    assert p.x == v1
-    assert p.y == v2
+    p = Point(x=i32(1), y=i32(2))
+    assert p.x == i32(1)
+    assert p.y == i32(2)
     #
     # uninitialized struct, will be used by gc_alloc
     p = Point()
     assert p._value == {"x": None, "y": None}
     assert p.x is None
     assert p.y is None
-    p.x = v3
-    p.y = v4
-    assert p.x == v3
-    assert p.y == v4
-    assert p._value == {"x": v3, "y": v4}
+    p.x = i32(3)
+    p.y = i32(4)
+    assert p.x == i32(3)
+    assert p.y == i32(4)
+    assert p._value == {"x": i32(3), "y": i32(4)}
 
 
 def test_nested_struct():
@@ -64,11 +57,11 @@ def test_nested_struct():
         a: Point
         b: Point
 
-    r = Rect(a=Point(x=v1, y=v2), b=Point(x=v3, y=v4))
-    assert r.a.x == v1
-    assert r.a.y == v2
-    assert r.b.x == v3
-    assert r.b.y == v4
+    r = Rect(a=Point(x=i32(1), y=i32(2)), b=Point(x=i32(3), y=i32(4)))
+    assert r.a.x == i32(1)
+    assert r.a.y == i32(2)
+    assert r.b.x == i32(3)
+    assert r.b.y == i32(4)
 
     # uninitialized
     r = Rect()
